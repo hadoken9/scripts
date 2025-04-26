ea46b44cc4a942fb21a4e81f1f27288e9f58cc07
hadoken9 <103473307+hadoken9@users.noreply.github.com>
2025-04-27 07:41:17 +0800

    Update template.js
    disable check config

--- surge/modules/sub-store-scripts/sing-box/template.js
+++ surge/modules/sub-store-scripts/sing-box/template.js
@@ -20,13 +20,13 @@ log(`传入参数 type: ${type}, name: ${name}, outbound: ${outbound}`)
 type = /^1$|col|组合/i.test(type) ? 'collection' : 'subscription'
 
 log(`① 解析配置文件`)
-let config
-try {
-  config = JSON.parse($content ?? $files[0])
-} catch (e) {
-  log(`${e.message ?? e}`)
-  throw new Error('配置文件不是合法的 JSON')
-}
+// let config
+// try {
+//   config = JSON.parse($content ?? $files[0])
+// } catch (e) {
+//   log(`${e.message ?? e}`)
+//   throw new Error('配置文件不是合法的 JSON')
+// }
 log(`② 获取订阅`)
 
 let proxies
