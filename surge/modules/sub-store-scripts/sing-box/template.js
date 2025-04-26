482f144d492eee628c8906cc07337a0c61e8f03c
hadoken9 <103473307+hadoken9@users.noreply.github.com>
2025-04-27 07:44:23 +0800

    Update template.js
    fix 解析配置文件

--- surge/modules/sub-store-scripts/sing-box/template.js
+++ surge/modules/sub-store-scripts/sing-box/template.js
@@ -20,7 +20,8 @@ log(`传入参数 type: ${type}, name: ${name}, outbound: ${outbound}`)
 type = /^1$|col|组合/i.test(type) ? 'collection' : 'subscription'
 
 log(`① 解析配置文件`)
-// let config
+let config
+config = JSON.parse($content ?? $files[0])
 // try {
 //   config = JSON.parse($content ?? $files[0])
 // } catch (e) {
